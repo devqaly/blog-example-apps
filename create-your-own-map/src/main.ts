602d4a3c31d89fb6e9fbe14f8c0eeeeecda9cb37
Bruno Francisco <bruno.francisco@codotto.com>
2024-02-01 16:36:51 -0300

    - added segments

--- create-your-own-map/src/main.ts
+++ create-your-own-map/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import { Graph } from "./services/Graph.ts";
 import { Point } from "./services/Point.ts";
+import { Segment } from "./services/Segment.ts";
 
 const canvas = document.querySelector('canvas')!
 
@@ -11,6 +12,9 @@ const p2 = new Point(500, 200)
 const p3 = new Point(400, 400)
 const p4 = new Point(100, 300)
 
-const graph = new Graph([p1, p2, p3, p4])
+const s1 = new Segment(p1, p2)
+const s2 = new Segment(p1, p3)
+
+const graph = new Graph([p1, p2, p3, p4], [s1, s2])
 
 graph.draw(context)
